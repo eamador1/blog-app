9e60e7cd5dd898e55fe9934a8b2cd46899126c99
Erwin Amador <eamador0305@gmail.com>
2023-12-13 15:57:15 -0600

    update property names

--- db/migrate/20231213211249_create_posts.rb
+++ db/migrate/20231213211249_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.references :author, null: false, foreign_key: {to_table: 'users'}, index: true
+      t.references :author_id, null: false, foreign_key: {to_table: 'users'}, index: true
       t.string :title
       t.string :text
       t.integer :comments_counter, default: 0
