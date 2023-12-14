eb7c23bed3242258ddf0776634b53ccd9db65117
Erwin Amador <eamador0305@gmail.com>
2023-12-14 17:26:56 -0600

    fix linter errors in post and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,10 @@ class User < ApplicationRecord
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
 
-  validates :name 
+  validates :name
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 end
-
-
