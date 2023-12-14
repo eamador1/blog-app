8624c5f0a87dfd1510ec17d839920ac53d5425d7
Erwin Amador <eamador0305@gmail.com>
2023-12-14 13:47:13 -0600

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :posts
 end
 
-  def three_recent_posts
-      post.order(created_at: :desc).limit(3)
-  end
+def three_recent_posts
+  post.order(created_at: :desc).limit(3)
+end
