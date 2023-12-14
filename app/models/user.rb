638e8d50d54ae74bde4b7d2dfd26e0a33067bcb0
Erwin Amador <eamador0305@gmail.com>
2023-12-14 16:13:24 -0600

    place method inside class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
+  
+  def three_recent_posts
+    post.order(created_at: :desc).limit(3)
+  end
 end
 
-def three_recent_posts
-  post.order(created_at: :desc).limit(3)
-end
+
