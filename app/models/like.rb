dc42381f4a07df7614046adcee4de140fd2ea8ea
Erwin Amador <eamador0305@gmail.com>
2023-12-14 11:41:13 -0600

    fix erronous migration

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
-class LikeSkip < ApplicationRecord
+class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
 end
--- db/migrate/20231214164142_create_like__skips.rb
+++
@@ -1,8 +0,0 @@
-class CreateLikeSkips < ActiveRecord::Migration[7.1]
-  def change
-    create_table :like__skips do |t|
-
-      t.timestamps
-    end
-  end
-end
