d6f64ded99af8d7c32d04fa3da89636b963c8bd7
Erwin Amador <eamador0305@gmail.com>
2023-12-15 10:54:06 -0600

    add tests to comment model

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Comment, type: :model do
     let(:post) { Post.create(title: 'I am tired', author: user) }
     it 'updates comments_counter' do
       Comment.create(user:, post:, text: 'Blah Blah Blsh')
-      # post.reload send(:update_comments_counter)
       expect(post.reload.comments_counter).to eq(1)
     end
   end
