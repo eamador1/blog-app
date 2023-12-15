3a8ec7f898f2855fcade431c9250c441fba5d1df
Erwin Amador <eamador0305@gmail.com>
2023-12-15 10:40:33 -0600

    improve validation, change attribute name not passing t test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
 
-  validates :name
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
