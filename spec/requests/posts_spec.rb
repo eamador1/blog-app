62b69e88198081d9113c863e525034f406c83ca3
Erwin Amador <eamador0305@gmail.com>
2023-12-20 22:12:25 -0600

    add unit test to post controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+  let(:user) do
+    User.create(name: 'John Doe')
+  end
+
+  let(:post) do
+    Post.create(title: 'Test Post', text: 'Lorem Ipsum', author: user)
+  end
+  # @post_path = "/users/#{@user.id}/posts"
+  # @post_id_path = "/users/#{@user.id}/posts/#{@post.id}"
+
+  describe 'GET /users/:user_id/posts' do
+    before { get user_posts_path(user) }
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'shows correct placeholder text in the response body' do
+      expect(response.body).to include('<h1>Posts Index</h1>')
+    end
+
+    it 'renders the index template' do
+      expect(response).to render_template(:index)
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET /users/:user_id/posts/:id' do
+    before { get user_post_path(user, post) }
+
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+    it 'shows correct placeholder text in the response body' do
+      expect(response.body).to include('<h1>Posts show</h1>')
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template(:show)
     end
   end
 end
