d7f3a01e3e74f550a9a98059185fd75df2d4aa56
Erwin Amador <eamador0305@gmail.com>
2024-01-11 20:49:59 -0600

    fix linter error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
-  
+
   def create
     @comment = Comment.new(comment_params)
     @comment.post = @post
