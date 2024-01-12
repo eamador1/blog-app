f2f5a2b278336c420b2d2f43b49c97a41d0e0e7e
Erwin Amador <eamador0305@gmail.com>
2024-01-12 09:43:16 -0600

    setup likes route in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
     post 'like', to: 'likes#create', on: :member, as:
-    :like_post
+    :user_post_likes
     resources :comments, only: [:create, :new]
 
 
