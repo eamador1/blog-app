1686484bb024344018392837ef6e5ffc12f9e875
Erwin Amador <eamador0305@gmail.com>
2024-01-12 10:35:25 -0600

    indentatecode lines correctly to visualize nesting better

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
     resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-    post 'like', to: 'likes#create', on: :member, as:
-    :user_post_likes
-    resources :comments, only: [:create, :new]
-
-
-    resources :likes, only: [:create]
+      resources :posts, only: [:index, :show, :new, :create] do
+        post 'like', to: 'likes#create', on: :member, as:
+        :user_post_likes
+        resources :comments, only: [:create, :new]
+        resources :likes, only: [:create]
 
   end 
   get 'posts', to: 'posts#index', on: :member, as:
