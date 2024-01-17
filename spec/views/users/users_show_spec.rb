4e4f077738f04f61950d3ade96064479bdd26716
Erwin Amador <eamador0305@gmail.com>
2024-01-16 20:45:21 -0600

    update number of post to make it reallistic

--- spec/views/users/users_show_spec.rb
+++ spec/views/users/users_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :system do
   describe 'user show page' do
     before do
-      @user = User.create(name: 'FirstUser', post_counter: 3, photo: 'https://picsum.photos/100', bio: 'Swiftie')
+      @user = User.create(name: 'FirstUser', post_counter: 4, photo: 'https://picsum.photos/100', bio: 'Swiftie')
       @post1 = Post.create(title: 'Hello 1', text: 'I am so glad to say hello 1', author_id: @user.id)
       @post2 = Post.create(title: 'Hello 2', text: 'I am so glad to say hello 2', author_id: @user.id)
       @post3 = Post.create(title: 'Hello 3', text: 'I am so glad to say hello 3', author_id: @user.id)
