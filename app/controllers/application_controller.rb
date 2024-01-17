a6005e4b8b0552e0bb2c9b6c63cd450d14cad9fd
Jose RM <mojica.raul@gmail.com>
2024-01-17 16:09:46 -0600

    Add before action in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  def setup_current_user
-    @current_user = current_user
-  end
-
-  def current_user
-    User.first
-  end
+  before_action :authenticate_user!
 end
