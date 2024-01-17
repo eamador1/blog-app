0f34dd884ec222108621c4acbfe849d9c61106b1
Erwin Amador <eamador0305@gmail.com>
2024-01-17 15:22:46 -0600

    make changes in relations in posts and comments database

--- db/migrate/20231213213658_create_comments.rb
+++ db/migrate/20231213213658_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: { to_table: 'users'}, index: true
-      t.references :post, null: false, foreign_key: { to_table: 'posts'}, index: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
       t.string :text
       t.timestamps
     end
