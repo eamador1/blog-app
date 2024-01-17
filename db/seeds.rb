b93920a53214c214739ae5ea7c66df9b5ac610cd
Jose RM <mojica.raul@gmail.com>
2024-01-17 16:08:56 -0600

    Add dummy data to populate the DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+    user = User.create(
+      photo: 'https://picsum.photos/100',
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      bio: Faker::Lorem.sentence,
+      password: 'password123'
+      
+    )
+    5.times do
+      new_post = Post.create(
+        author: user,
+        title: Faker::Lorem.sentence,
+        text: Faker::Lorem.paragraph
+   
+        )
+        10.times do
+          Comment.create(
+            post: new_post,
+            user_id: user.id,
+            text: Faker::Lorem.sentence
+          )
+        end
+      end
+end  
