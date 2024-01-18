3a3ca8d159d257cb5e3e75fc331f00804292a0d9
Jose RM <mojica.raul@gmail.com>
2024-01-18 13:21:29 -0600

    Added roles and is? method in the user model file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
+
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
