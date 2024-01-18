b275697e66617d97be2366885578377209070cbf
Erwin Amador <eamador0305@gmail.com>
2024-01-18 11:50:12 -0600

    fix linter error

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
 10.times do
     user = User.create(
       photo: 'https://picsum.photos/100',
