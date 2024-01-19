03bef40d69e3c1bf9d8443c730f399fcd603124d
Erwin Amador <eamador0305@gmail.com>
2024-01-18 23:49:02 -0600

    assocition including dependent: :destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   after_save :update_posts_counter
@@ -13,8 +13,6 @@ class Post < ApplicationRecord
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-
-  private
 
   def update_posts_counter
     author.update(post_counter: author.posts.count)
